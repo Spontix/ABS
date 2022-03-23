7e7fc39260394a5cdc2046a2ca0b2a291bb32da0
Eden Azulay <edenaz@mta.ac.il>
2022-03-23 12:32:14 +0200

    Menu in application.console

--- ui/src/application/console/Main.java
+++ ui/src/application/console/Main.java
@@ -5,5 +5,15 @@ public class Main {
     public static void main(String[] args)
     {
     }
+    /*
+    Menu:
+1. Reading the system information file.                                  Do Eliran
+2. Presentation of information on existing loans and their status.       Do Eden
+3. Display information about system customers.
+4. Loading funds into the account.
+5. Withdrawal of funds from the account.
+6. Inlay activation.
+7. Promoting the timeline and providing payments.
+8. Exiting the system.*/
 
 }
